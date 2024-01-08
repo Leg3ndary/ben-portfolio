cb153944b23b6bb567f23f39c1a1380b119ef33c
Leg3ndary <ben.zhou.2006@gmail.com>
2024-01-08 13:16:03 -0500

    Add hashing woo

---
+++ src/pages/api/getColor/[hash].ts
@@ -0,0 +1,28 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getColorFromURL } from "color-thief-node";
+
+type Data = {
+    answer: string;
+};
+
+function rgbToHex(rgb: number[]) {
+    const hex =
+        "#" +
+        rgb.map((x) => {
+            const hex = x.toString(16);
+            return hex.length === 1 ? "0" + hex : hex;
+        });
+    return hex.replaceAll(",", "");
+}
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
+) {
+    const hash = req.query.hash as string;
+    const url = `https://i.scdn.co/image/${hash}`;
+
+    const hex = rgbToHex(await getColorFromURL(url));
+
+    res.status(200).json({ answer: hex });
+}
