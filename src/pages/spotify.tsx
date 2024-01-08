a2a9bc9aebc93ef4c3c6c8e6f50ae7bdb7a890fe
_Leg3ndary <ben.zhou.2006@gmail.com>
2024-01-07 22:51:55 -0500

    Add spotify

---
+++ src/pages/spotify.tsx
@@ -0,0 +1,8 @@
+export default function Spotify() {
+    // So people can see my amazing music taste
+    return (
+        <div className="w-full h-full bg-black">
+            <h1 className="text-4xl font-bold text-center text-white">Sorry this page isn&apos;t finished yet...</h1>
+        </div>
+    )
+}
