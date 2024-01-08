9f96ed12aec74b868c43a1dcd6d115761f680501
_Leg3ndary <ben.zhou.2006@gmail.com>
2024-01-08 16:14:21 -0500

    Try a bunch of changes

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    legacyBrowsers: false,
-    outputFileTracingRoot: path.join(__dirname, "../../"),
-    outputFileTracingExcludes: {
-        "*": [
-            "node_modules/next/wasm"
-        ],
-    },
 };
 
 module.exports = nextConfig;
