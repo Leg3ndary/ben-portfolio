eed314a4f82b375b23a25285f5ded7bcd23bc0bf
_Leg3ndary <ben.zhou.2006@gmail.com>
2024-06-19 22:14:32 -0400

    Adjust blog post style

--- src/pages/projects/macroboard.tsx
+++ src/pages/projects/macroboard.tsx
@@ -33,30 +33,7 @@ export default function Macroboard() {
                     content="View my process for building my very own macroboard."
                 />
             </Head>
-            <div className="relative top-0 flex justify-center w-full h-[550px] bg-rainbow-gradient animate-breathing-gradient">
-                <motion.div
-                    className="relative flex h-[370px] lg:h-[300px] bg-white border-black w-11/12 lg:w-[1000px] drop-shadow-2xl mt-32 lg:mt-40 rounded-3xl duration-1000 ease-in-out transition-all"
-                    initial={{ y: -20, opacity: 0 }}
-                    animate={{ y: 0, opacity: 1 }}
-                    transition={{ duration: 1 }}
-                >
-                    <div className="flex flex-col justify-center w-full h-full p-12">
-                        <h2 className="p-2 text-lg text-center">
-                            BUILDING MY VERY OWN CUSTOM MACROKEYBOARD
-                        </h2>
-                        <h1 className="p-2 text-4xl font-black text-center lg:text-6xl">
-                            SPOTIFY MACROBOARD
-                        </h1>
-                        <p className="p-2 py-5 font-light">
-                            For my Grade 12 Computer Engineering final
-                            performance task I decided to build my very own
-                            custom macrokeyboard. This page goes through the fun
-                            process of figuring out what I needed to do to make
-                            this project a reality.
-                        </p>
-                    </div>
-                </motion.div>
-            </div>
+            <div className="relative top-0 flex justify-center w-full h-32 bg-rainbow-gradient animate-breathing-gradient" />
             <div className="flex mx-auto w-[400px] md:w-[700px] lg:w-[1000px] xl:[1200px] mt-12 mb-16 lg:mb-10 lg:mt-8 p-2 lg:p-4">
                 <div className="flex flex-col w-full px-6 ">
                     <h1 className="py-1 text-4xl font-black">
