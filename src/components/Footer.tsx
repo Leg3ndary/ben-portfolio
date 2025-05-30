050b110ce6e4c4ebe79cc36e5eeb341ae473ca99
Ben Zhou <ben.zhou.2006@gmail.com>
2025-05-30 14:11:23 -0400

    Adjust footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -25,8 +25,10 @@ export default function Footer() {
                                 Contacts
                             </h2>
                             <p className="text-[#777777] py-2">
-                                Thanks for reading through my website! If you
-                                want to contact me please reach out on LinkedIn.
+                                Thanks for visiting my website!
+                            </p>
+                            <p className="text-[#777777] py-2">
+                                If you have any further questions or just want to contact me, please reach out to me through LinkedIn or Email.
                             </p>
                             <p className="text-[#777777] py-2">
                                 Copyright © 2023-2024 Ben Zhou All rights
@@ -42,52 +44,52 @@ export default function Footer() {
                             </p>
                             <div className="flex gap-4">
                                 <motion.div {...motionProps}>
-                                    <div className="p-1 transition border border-white rounded-md group hover:border-transparent">
+                                    <div className="p-1 transition border border-transparent rounded-md group hover:border-transparent">
                                         <a
                                             href="https://www.linkedin.com/in/ben-zhou06/"
                                             target="_blank"
                                         >
-                                            <FaLinkedin className="w-6 h-6 lg:w-8 lg:h-8 text-white transition hover:text-[#0077B5]" />
+                                            <FaLinkedin className="w-6 h-6 lg:w-8 lg:h-8 text-[#AAAAAA] transition hover:text-[#0077B5]" />
                                         </a>
                                     </div>
                                 </motion.div>
                                 <motion.div {...motionProps}>
-                                    <div className="p-1 transition border border-white rounded-md group hover:border-transparent">
+                                    <div className="p-1 transition border border-transparent rounded-md group hover:border-transparent">
                                         <a
                                             href="https://github.com/Leg3ndary"
                                             target="_blank"
                                         >
-                                            <ImGithub className="w-6 h-6 lg:w-8 lg:h-8 transition hover:text-[#9f7be1] text-white" />
+                                            <ImGithub className="w-6 h-6 lg:w-8 lg:h-8 transition hover:text-[#9f7be1] text-[#AAAAAA]" />
                                         </a>
                                     </div>
                                 </motion.div>
                                 <motion.div {...motionProps}>
-                                    <div className="p-1 transition border border-white rounded-md group hover:border-transparent">
+                                    <div className="p-1 transition border border-transparent rounded-md group hover:border-transparent">
                                         <a
                                             href="https://discord.com/users/360061101477724170"
                                             target="_blank"
                                         >
-                                            <FaDiscord className="w-6 h-6 lg:w-8 lg:h-8 text-white transition hover:text-[#7289DA]" />
+                                            <FaDiscord className="w-6 h-6 lg:w-8 lg:h-8 text-[#AAAAAA] transition hover:text-[#7289DA]" />
                                         </a>
                                     </div>
                                 </motion.div>
                                 <motion.div {...motionProps}>
-                                    <div className="p-1 transition border border-white rounded-md group hover:border-transparent">
+                                    <div className="p-1 transition border border-transparent rounded-md group hover:border-transparent">
                                         <a
                                             href="https://www.instagram.com/bennyz_06/"
                                             target="_blank"
                                         >
-                                            <FaInstagram className="w-6 h-6 lg:w-8 lg:h-8 text-white transition hover:text-[#fd1d1d]" />
+                                            <FaInstagram className="w-6 h-6 lg:w-8 lg:h-8 text-[#AAAAAA] transition hover:text-[#fd1d1d]" />
                                         </a>
                                     </div>
                                 </motion.div>
                                 <motion.div {...motionProps}>
-                                    <div className="p-1 transition border border-white rounded-md group hover:border-transparent">
+                                    <div className="p-1 transition border border-transparent rounded-md group hover:border-transparent">
                                         <a
                                             href="https://monkeytype.com/profile/_Leg3ndary"
                                             target="_blank"
                                         >
-                                            <SiMonkeytype className="w-6 h-6 lg:w-8 lg:h-8 text-white transition hover:text-[#e2b714]" />
+                                            <SiMonkeytype className="w-6 h-6 lg:w-8 lg:h-8 text-[#AAAAAA] transition hover:text-[#e2b714]" />
                                         </a>
                                     </div>
                                 </motion.div>
